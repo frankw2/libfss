21504660b9460507390ca949c623f2895fa240bb
Frank Wang <frankw@cs.stanford.edu>
2016-12-29 22:34:34 -0800

    basics there, but not quite there yet

--- libfss/fss_2party.go
+++ libfss/fss_2party.go
@@ -9,37 +9,37 @@ import (
 
 type Fss struct {
 	// store keys used in fixedBlocks so that they can be sent to the server
-	prfKeys     [][]byte
-	fixedBlocks []cipher.Block
+	PrfKeys     [][]byte
+	FixedBlocks []cipher.Block
 	N           uint
-	numBits     uint   // number of bits in domain
-	temp        []byte // temporary slices so that we only need to allocate memory at the beginning
-	out         []byte
+	NumBits     uint   // number of bits in domain
+	Temp        []byte // temporary slices so that we only need to allocate memory at the beginning
+	Out         []byte
 }
 
 type FssKey struct {
-	sInit   []byte
-	tInit   byte
-	cw      [][]byte // there are n
-	finalCW int
+	SInit   []byte
+	TInit   byte
+	CW      [][]byte // there are n
+	FinalCW int
 }
 
 const initPRFLen int = 4
 
 // initialize client with this function
 func (f Fss) ClientInitialize(numBits uint) {
-	f.numBits = numBits
-	f.prfKeys = make([][]byte, initPRFLen)
+	f.NumBits = numBits
+	f.PrfKeys = make([][]byte, initPRFLen)
 	// Create fixed AES blocks
-	f.fixedBlocks = make([]cipher.Block, initPRFLen)
+	f.FixedBlocks = make([]cipher.Block, initPRFLen)
 	for i := 0; i < initPRFLen; i++ {
-		f.prfKeys[i] = make([]byte, aes.BlockSize)
-		rand.Read(f.prfKeys[i])
-		block, err := aes.NewCipher(f.prfKeys[i])
+		f.PrfKeys[i] = make([]byte, aes.BlockSize)
+		rand.Read(f.PrfKeys[i])
+		block, err := aes.NewCipher(f.PrfKeys[i])
 		if err != nil {
 			panic(err.Error())
 		}
-		f.fixedBlocks[i] = block
+		f.FixedBlocks[i] = block
 	}
 	var x uint64 = 1 << 32
 	if uint(x) == 0 {
@@ -47,22 +47,22 @@ func (f Fss) ClientInitialize(numBits uint) {
 	} else {
 		f.N = 64
 	}
-	f.temp = make([]byte, aes.BlockSize)
-	f.out = make([]byte, aes.BlockSize*4)
+	f.Temp = make([]byte, aes.BlockSize)
+	f.Out = make([]byte, aes.BlockSize*4)
 }
 
 // upon receiving query from server, initialize server with
 // this function. The server, unlike the client
 // receives prfKeys, so it doesn't need to pick random ones
 func (f Fss) ServerInitialize(prfKeys [][]byte, numBits uint) {
-	f.numBits = numBits
+	f.NumBits = numBits
 	for i := range prfKeys {
-		f.prfKeys[i] = prfKeys[i]
-		block, err := aes.NewCipher(f.prfKeys[i])
+		f.PrfKeys[i] = prfKeys[i]
+		block, err := aes.NewCipher(f.PrfKeys[i])
 		if err != nil {
 			panic(err.Error())
 		}
-		f.fixedBlocks[i] = block
+		f.FixedBlocks[i] = block
 	}
 	var x uint64 = 1 << 32
 	if uint(x) == 0 {
@@ -78,37 +78,37 @@ func (f Fss) GenerateTreePF(a uint, b uint) []FssKey {
 	// Set up initial values
 	tempRand1 := make([]byte, aes.BlockSize+1)
 	rand.Read(tempRand1)
-	fssKeys[0].sInit = tempRand1[:aes.BlockSize]
-	fssKeys[0].tInit = tempRand1[aes.BlockSize] % 2
-	fssKeys[1].sInit = make([]byte, aes.BlockSize)
-	rand.Read(fssKeys[1].sInit)
-	fssKeys[1].tInit = (fssKeys[0].tInit + 1) % 2
+	fssKeys[0].SInit = tempRand1[:aes.BlockSize]
+	fssKeys[0].TInit = tempRand1[aes.BlockSize] % 2
+	fssKeys[1].SInit = make([]byte, aes.BlockSize)
+	rand.Read(fssKeys[1].SInit)
+	fssKeys[1].TInit = (fssKeys[0].TInit + 1) % 2
 
 	// Set current seed being used
-	sCurr0 := fssKeys[0].sInit
-	sCurr1 := fssKeys[1].sInit
-	tCurr0 := fssKeys[0].tInit
-	tCurr1 := fssKeys[1].tInit
+	sCurr0 := fssKeys[0].SInit
+	sCurr1 := fssKeys[1].SInit
+	tCurr0 := fssKeys[0].TInit
+	tCurr1 := fssKeys[1].TInit
 
 	// Initialize correction words in FSS keys
-	fssKeys[0].cw = make([][]byte, f.numBits)
-	fssKeys[1].cw = make([][]byte, f.numBits)
-	for i := uint(0); i < f.numBits; i++ {
+	fssKeys[0].CW = make([][]byte, f.NumBits)
+	fssKeys[1].CW = make([][]byte, f.NumBits)
+	for i := uint(0); i < f.NumBits; i++ {
 		// make AES block size + 2 bytes
-		fssKeys[0].cw[i] = make([]byte, aes.BlockSize+2)
-		fssKeys[1].cw[i] = make([]byte, aes.BlockSize+2)
+		fssKeys[0].CW[i] = make([]byte, aes.BlockSize+2)
+		fssKeys[1].CW[i] = make([]byte, aes.BlockSize+2)
 	}
 
 	leftStart := 0
 	rightStart := aes.BlockSize + 1
-	for i := uint(0); i < f.numBits; i++ {
+	for i := uint(0); i < f.NumBits; i++ {
 		// "expand" seed into two seeds + 2 bits
-		prf(sCurr0, f.fixedBlocks, 3, f.temp, f.out)
+		prf(sCurr0, f.FixedBlocks, 3, f.Temp, f.Out)
 		prfOut0 := make([]byte, aes.BlockSize*3)
-		copy(prfOut0, f.out[:aes.BlockSize*3])
-		prf(sCurr1, f.fixedBlocks, 3, f.temp, f.out)
+		copy(prfOut0, f.Out[:aes.BlockSize*3])
+		prf(sCurr1, f.FixedBlocks, 3, f.Temp, f.Out)
 		prfOut1 := make([]byte, aes.BlockSize*3)
-		copy(prfOut1, f.out[:aes.BlockSize*3])
+		copy(prfOut1, f.Out[:aes.BlockSize*3])
 
 		// Parse out "t" bits
 		t0Left := prfOut0[aes.BlockSize] % 2
@@ -127,21 +127,21 @@ func (f Fss) GenerateTreePF(a uint, b uint) []FssKey {
 		}
 		// Set correction words for both keys. Note: they are the same
 		for j := 0; j < aes.BlockSize; j++ {
-			fssKeys[0].cw[i][j] = prfOut0[lose+j] ^ prfOut1[lose+j]
-			fssKeys[1].cw[i][j] = fssKeys[0].cw[i][j]
+			fssKeys[0].CW[i][j] = prfOut0[lose+j] ^ prfOut1[lose+j]
+			fssKeys[1].CW[i][j] = fssKeys[0].CW[i][j]
 		}
-		fssKeys[0].cw[i][aes.BlockSize] = t0Left ^ t1Left ^ aBit ^ 1
-		fssKeys[1].cw[i][aes.BlockSize] = fssKeys[0].cw[i][aes.BlockSize]
-		fssKeys[0].cw[i][aes.BlockSize+1] = t0Right ^ t1Right ^ aBit
-		fssKeys[1].cw[i][aes.BlockSize+1] = fssKeys[0].cw[i][aes.BlockSize+1]
+		fssKeys[0].CW[i][aes.BlockSize] = t0Left ^ t1Left ^ aBit ^ 1
+		fssKeys[1].CW[i][aes.BlockSize] = fssKeys[0].CW[i][aes.BlockSize]
+		fssKeys[0].CW[i][aes.BlockSize+1] = t0Right ^ t1Right ^ aBit
+		fssKeys[1].CW[i][aes.BlockSize+1] = fssKeys[0].CW[i][aes.BlockSize+1]
 
 		for j := 0; j < aes.BlockSize; j++ {
-			sCurr0[j] = prfOut0[keep+j] ^ (tCurr0 * fssKeys[0].cw[i][j])
-			sCurr1[j] = prfOut1[keep+j] ^ (tCurr1 * fssKeys[0].cw[i][j])
+			sCurr0[j] = prfOut0[keep+j] ^ (tCurr0 * fssKeys[0].CW[i][j])
+			sCurr1[j] = prfOut1[keep+j] ^ (tCurr1 * fssKeys[0].CW[i][j])
 		}
-		tCWKeep := fssKeys[0].cw[i][aes.BlockSize]
+		tCWKeep := fssKeys[0].CW[i][aes.BlockSize]
 		if keep == rightStart {
-			tCWKeep = fssKeys[0].cw[i][aes.BlockSize+1]
+			tCWKeep = fssKeys[0].CW[i][aes.BlockSize+1]
 		}
 		tCurr0 = (prfOut0[keep+aes.BlockSize] % 2) ^ (tCWKeep * tCurr0)
 		tCurr1 = (prfOut1[keep+aes.BlockSize] % 2) ^ (tCWKeep * tCurr1)
@@ -149,33 +149,33 @@ func (f Fss) GenerateTreePF(a uint, b uint) []FssKey {
 	// Convert final CW to integer
 	sFinal0, _ := binary.Varint(sCurr0[:8])
 	sFinal1, _ := binary.Varint(sCurr1[:8])
-	fssKeys[0].finalCW = int(b) - int(sFinal0) + int(sFinal1)
-	fssKeys[1].finalCW = -1 * fssKeys[0].finalCW
+	fssKeys[0].FinalCW = int(b) - int(sFinal0) + int(sFinal1)
+	fssKeys[1].FinalCW = -1 * fssKeys[0].FinalCW
 	return fssKeys
 }
 
-func (f Fss) evaluatePF(b byte, k FssKey, x uint) int {
-	sCurr := k.sInit
-	tCurr := k.tInit
-	for i := uint(0); i < f.numBits; i++ {
-		prf(sCurr, f.fixedBlocks, 3, f.temp, f.out)
+func (f Fss) EvaluatePF(b byte, k FssKey, x uint) int {
+	sCurr := k.SInit
+	tCurr := k.TInit
+	for i := uint(0); i < f.NumBits; i++ {
+		prf(sCurr, f.FixedBlocks, 3, f.Temp, f.Out)
 		for j := 0; j < aes.BlockSize+2; j++ {
-			f.out[j] = f.out[j] ^ (tCurr * k.cw[i][j])
+			f.Out[j] = f.Out[j] ^ (tCurr * k.CW[i][j])
 		}
 		xBit := getBit(x, i, f.N)
 		// Pick right seed expansion based on
 		if xBit == 0 {
-			sCurr = f.out[:aes.BlockSize]
-			tCurr = f.out[aes.BlockSize]
+			sCurr = f.Out[:aes.BlockSize]
+			tCurr = f.Out[aes.BlockSize]
 		} else {
-			sCurr = f.out[(aes.BlockSize + 1):(aes.BlockSize*2 + 1)]
-			tCurr = f.out[aes.BlockSize*2+1]
+			sCurr = f.Out[(aes.BlockSize + 1):(aes.BlockSize*2 + 1)]
+			tCurr = f.Out[aes.BlockSize*2+1]
 		}
 	}
 	sFinal, _ := binary.Varint(sCurr[:8])
 	if b == 0 {
-		return int(sFinal) + int(tCurr)*k.finalCW
+		return int(sFinal) + int(tCurr)*k.FinalCW
 	} else {
-		return -1 * (int(sFinal) + int(tCurr)*k.finalCW)
+		return -1 * (int(sFinal) + int(tCurr)*k.FinalCW)
 	}
 }
--- test_fss/test_fss.go
+++ test_fss/test_fss.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
 	"github.com/frankw2/libfss"
 )
 
 func main() {
-	f := new(libfss.Fss)
-	f.ClientInitialize(10)
-	//fssKeys := f.GenerateTreePF(10, 1)
+	// Generate fss Keys on client
+	fClient := new(libfss.Fss)
+	fClient.ClientInitialize(10)
+	fssKeys := fClient.GenerateTreePF(10, 5)
+
+	// Simulate server
+	fServer := new(libfss.Fss)
+	fServer.ServerInitialize(fClient.PrfKeys, fClient.NumBits)
+	ans0 := fServer.EvaluatePF(0, fssKeys[0], 10)
+	ans1 := fServer.EvaluatePF(1, fssKeys[1], 10)
+	fmt.Println(ans0 + ans1)
 }
