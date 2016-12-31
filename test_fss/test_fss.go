e77be0d651c385ada7b5cb8b828057c4b0ce69e7
Frank Wang <frankw@cs.stanford.edu>
2016-12-30 16:36:29 -0800

    nits

--- test_fss/test_fss.go
+++ test_fss/test_fss.go
@@ -14,9 +14,9 @@ func main() {
 	fServer := libfss.ServerInitialize(fClient.PrfKeys, fClient.NumBits)
 
 	var ans0, ans1 int = 0, 0
-	//ans0 = fServer.EvaluatePF(0, fssKeys[0], 10)
-	//ans1 = fServer.EvaluatePF(1, fssKeys[1], 10)
-	//fmt.Println("Match (should be non-zero):", ans0+ans1)
+	ans0 = fServer.EvaluatePF(0, fssKeys[0], 10)
+	ans1 = fServer.EvaluatePF(1, fssKeys[1], 10)
+	fmt.Println("Match (should be non-zero):", ans0+ans1)
 
 	ans0 = fServer.EvaluatePF(0, fssKeys[0], 11)
 	ans1 = fServer.EvaluatePF(1, fssKeys[1], 11)
